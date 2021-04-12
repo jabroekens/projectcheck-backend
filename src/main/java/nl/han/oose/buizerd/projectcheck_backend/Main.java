7dff9f3cad9dae1e028e4216e5c066af005f0598
JA Broekens <JA.Broekens@student.han.nl>
2021-04-12 16:48:24 +0200

    add example

---
+++ src/main/java/nl/han/oose/buizerd/projectcheck_backend/Main.java
@@ -0,0 +1,10 @@
+package nl.han.oose.buizerd.projectcheck_backend;
+
+public class Main {
+
+	public static void main(String[] args) {
+		var john = new Person("John");
+		System.out.println("Hello, " + john.getName());
+	}
+
+}
