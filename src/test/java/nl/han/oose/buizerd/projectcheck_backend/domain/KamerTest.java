943ab9fd99c66e929b39018bbaade2a156d30b2b
JA Broekens <JA.Broekens@student.han.nl>
2021-04-28 17:18:27 +0200

    add KamerTest.java

---
+++ src/test/java/nl/han/oose/buizerd/projectcheck_backend/domain/KamerTest.java
@@ -0,0 +1,34 @@
+package nl.han.oose.buizerd.projectcheck_backend.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+public class KamerTest {
+
+	private Kamer kamer;
+
+	@BeforeEach
+	void init() {
+		kamer = new Kamer();
+	}
+
+	@Test
+	void genereertUniekeCode() {
+		try (MockedStatic<Kamer> mock = Mockito.mockStatic(Kamer.class)) {
+			mock.when(Kamer::genereerCode).thenReturn("123456");
+			Assertions.assertEquals(Kamer.genereerCode(), "123456");
+		}
+	}
+
+	@Test
+	void zetJuisteBegeleider() {
+		Begeleider begeleider = new Begeleider(new DeelnemerId("123456"), "Joost");
+		Assertions.assertNull(kamer.getBegeleider());
+		kamer.setBegeleider(begeleider);
+		Assertions.assertEquals(kamer.getBegeleider(), begeleider);
+	}
+
+}
