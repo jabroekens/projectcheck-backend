1a10f6f0f89d9736b34890e0fe94f3eb6ed28464
JA Broekens <JA.Broekens@student.han.nl>
2021-05-07 15:29:04 +0200

    Event.java: use `RuntimeTypeAdapterFactory` instead of own deserializer
    Stumbled upon this while browsing, and decided this was the better option because:
    * It's been tested already.
    * No point in reinventing the wheel.
    * Less maintenance/technical debt for the application (see above).
    
    In addition, I'm using the Reflection library to ensure every subclass of `Event`
    gets registered with `RuntimeTypeAdapterFactory` automatically, making the code
    more DRY.
    
    See: https://github.com/google/gson/blob/master/extras/src/main/java/com/google/gson/typeadapters/RuntimeTypeAdapterFactory.java

--- src/main/java/nl/han/oose/buizerd/projectcheck_backend/event/Event.java
+++ src/main/java/nl/han/oose/buizerd/projectcheck_backend/event/Event.java
@@ -2,13 +2,8 @@ package nl.han.oose.buizerd.projectcheck_backend.event;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
-import java.util.HashMap;
-import java.util.Map;
+import com.google.gson.typeadapters.RuntimeTypeAdapterFactory;
 import java.util.concurrent.CompletableFuture;
 import javax.validation.constraints.NotNull;
 import javax.websocket.EndpointConfig;
@@ -17,30 +12,12 @@ import nl.han.oose.buizerd.projectcheck_backend.domain.Deelnemer;
 import nl.han.oose.buizerd.projectcheck_backend.domain.DeelnemerId;
 import nl.han.oose.buizerd.projectcheck_backend.domain.Kamer;
 import nl.han.oose.buizerd.projectcheck_backend.repository.KamerRepository;
+import org.reflections.Reflections;
 
 /**
  * Representeert een event.
  */
 public abstract class Event {
-
-	private static final EventDeserializer EVENT_DESERIALIZER;
-	private static final Gson GSON;
-
-	static {
-		EVENT_DESERIALIZER = new EventDeserializer();
-		GSON = new GsonBuilder().registerTypeAdapter(Event.class, Event.EVENT_DESERIALIZER).create();
-	}
-
-	/**
-	 * Registreert een eventklasse.
-	 *
-	 * @param eventKlasse De eventklasse.
-	 */
-	protected static void registreerEvent(@NotNull Class<? extends Event> eventKlasse) {
-		// Verandert een klassenaam als "FooBarEvent" naar "FOO_BAR"
-		String eventNaam = eventKlasse.getSimpleName().replace("Event", "").replaceAll("(?<!^)(?=[A-Z])", "_").toUpperCase();
-		Event.EVENT_DESERIALIZER.registreerKlasse(eventNaam, eventKlasse);
-	}
 
 	private DeelnemerId deelnemer;
 
@@ -95,12 +72,30 @@ public abstract class Event {
 	 */
 	public static class Decoder implements javax.websocket.Decoder.Text<Event> {
 
+		private static final Gson GSON;
+
+		static {
+			RuntimeTypeAdapterFactory<Event> eventAdapterFactory = RuntimeTypeAdapterFactory.of(Event.class, "eventNaam");
+
+			Reflections reflections = new Reflections(Event.class.getPackage().getName());
+			reflections.getSubTypesOf(Event.class).forEach(
+				event -> {
+					String eventNaam = event.getSimpleName().replace("Event", "").replaceAll("(?<!^)(?=[A-Z])", "_").toUpperCase();
+					eventAdapterFactory.registerSubtype(event, eventNaam);
+				}
+			);
+
+			GSON = new GsonBuilder().registerTypeAdapterFactory(eventAdapterFactory).create();
+		}
+
+		private Event event;
+
 		/**
 		 * {@inheritDoc}
 		 */
 		@Override
 		public Event decode(String s) {
-			return Event.GSON.fromJson(s, Event.class);
+			return event;
 		}
 
 		/**
@@ -108,7 +103,13 @@ public abstract class Event {
 		 */
 		@Override
 		public boolean willDecode(String s) {
-			return s != null && !s.isEmpty();
+			// Probeer te deserializeren en cache het resultaat als het wel lukt.
+			try {
+				event = Decoder.GSON.fromJson(s, Event.class);
+			} catch (JsonParseException e) {
+				return false;
+			}
+			return true;
 		}
 
 		/**
@@ -125,47 +126,6 @@ public abstract class Event {
 		@Override
 		public void destroy() {
 			// Wordt niet gebruikt
-		}
-
-	}
-
-	/**
-	 * Deserializeert een {@link Event}.
-	 *
-	 * @see com.google.gson.JsonDeserializer
-	 */
-	private static class EventDeserializer implements JsonDeserializer<Event> {
-
-		private final Map<String, Class<? extends Event>> eventKlassen;
-
-		private EventDeserializer() {
-			eventKlassen = new HashMap<>();
-		}
-
-		private void registreerKlasse(@NotNull String eventNaam, @NotNull Class<? extends Event> eventKlasse) {
-			this.eventKlassen.put(eventNaam, eventKlasse);
-		}
-
-		/**
-		 * {@inheritDoc}
-		 */
-		@Override
-		public Event deserialize(JsonElement json, java.lang.reflect.Type typeOfT, JsonDeserializationContext context)
-		throws JsonParseException {
-			JsonObject jsonObject = json.getAsJsonObject();
-			if (jsonObject.has("eventNaam")) {
-				String eventNaam = jsonObject.get("eventNaam").getAsString();
-
-				if (eventKlassen.containsKey(eventNaam)) {
-					return context.deserialize(jsonObject, eventKlassen.get(eventNaam));
-				} else {
-					// XXX Hier een aparte exception-klasse voor aanmaken? Denk ook aan de front-end!
-					throw new RuntimeException("Value of 'eventNaam' is not recognized.");
-				}
-			} else {
-				// XXX Is dit de juiste manier om JsonParseException te gebruiken? Zie ook comment hierboven
-				throw new JsonParseException("Key 'eventNaam' not present.");
-			}
 		}
 
 	}
