e3d53598dc1ab36dda338d06cd579cf7aa9b5853
JA Broekens <JA.Broekens@student.han.nl>
2021-05-13 01:47:57 +0200

    CORSFilter.java: configured headers

--- src/main/java/nl/han/oose/buizerd/projectcheck_backend/CORSFilter.java
+++ src/main/java/nl/han/oose/buizerd/projectcheck_backend/CORSFilter.java
@@ -10,17 +10,8 @@ public class CORSFilter implements ContainerResponseFilter {
 
 	@Override
 	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
-		/*
-		 * TODO Naar veilige waarden zetten
-		 * https://stackoverflow.com/a/23631475
-		 */
 		responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-		responseContext.getHeaders().add(
-			"Access-Control-Allow-Headers",
-			"origin, content-type, accept, authorization"
-		);
-		responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-		responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+		responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, HEAD");
 	}
 
 }
