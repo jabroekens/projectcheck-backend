82a3f88959155197b2f240e09e3b6ecb328af9ea
JA Broekens <JA.Broekens@student.han.nl>
2021-05-19 17:23:08 +0200

    removed unused `OngeldigeNaamException` class

--- src/main/java/nl/han/oose/buizerd/projectcheck_backend/exception/OngeldigeNaamException.java
+++
@@ -1,9 +0,0 @@
-package nl.han.oose.buizerd.projectcheck_backend.exception;
-
-public class OngeldigeNaamException extends IllegalArgumentException {
-
-	public OngeldigeNaamException(String naam) {
-		super("Ongeldige naam: " + naam);
-	}
-
-}
