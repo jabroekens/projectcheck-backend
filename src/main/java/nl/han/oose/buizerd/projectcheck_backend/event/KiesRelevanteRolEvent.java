cb06d7701a13e9fbcade7f589a3e6e2750fa360c
JA Broekens <JA.Broekens@student.han.nl>
2021-05-19 13:14:55 +0200

    KiesRelevanteRolEvent.java: remove `deelnemerId` field

--- src/main/java/nl/han/oose/buizerd/projectcheck_backend/event/KiesRelevanteRolEvent.java
+++ src/main/java/nl/han/oose/buizerd/projectcheck_backend/event/KiesRelevanteRolEvent.java
@@ -1,10 +1,8 @@
 package nl.han.oose.buizerd.projectcheck_backend.event;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.websocket.Session;
 import nl.han.oose.buizerd.projectcheck_backend.domain.Begeleider;
-import nl.han.oose.buizerd.projectcheck_backend.domain.DeelnemerId;
 import nl.han.oose.buizerd.projectcheck_backend.domain.Kamer;
 import nl.han.oose.buizerd.projectcheck_backend.domain.Rol;
 
@@ -13,21 +11,17 @@ public class KiesRelevanteRolEvent extends Event {
 	@NotNull
 	String rolNaam;
 
-	@NotNull
-	@Valid
-	DeelnemerId deelnemerId;
-
 	@Override
 	protected EventResponse voerUit(Kamer kamer, Session session) {
 		try {
 			Rol rol = Rol.valueOf(rolNaam);
 			Begeleider begeleider = kamer.getBegeleider();
-			if (begeleider.getDeelnemerId().equals(deelnemerId)) {
+			if (begeleider.getDeelnemerId().equals(super.getDeelnemerId())) {
 				kamer.activeerRelevanteRol(rol);
 				String bericht = String.format("de rol %s is ingeschakeld voor de kamer %s", rol, kamer.getKamerCode());
 				return new EventResponse(EventResponse.Status.OK).metContext("bericht", bericht);
 			} else {
-				return new EventResponse(EventResponse.Status.VERBODEN).metContext("deelnemer", deelnemerId);
+				return new EventResponse(EventResponse.Status.VERBODEN).metContext("deelnemer", super.getDeelnemerId());
 			}
 		} catch (IllegalArgumentException e) {
 			return new EventResponse(EventResponse.Status.ROL_NIET_GEVONDEN).metContext("rolNaam", rolNaam);
