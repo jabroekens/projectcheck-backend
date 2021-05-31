ff37fbc05d1ccb375407558a26281170f0db262f
JA Broekens <JA.Broekens@student.han.nl>
2021-05-31 17:14:18 +0200

    add VolgendeFaseEvent

---
+++ src/main/java/nl/han/oose/buizerd/projectcheck_backend/event/VolgendeFaseEvent.java
@@ -0,0 +1,37 @@
+package nl.han.oose.buizerd.projectcheck_backend.event;
+
+import jakarta.websocket.Session;
+import nl.han.oose.buizerd.projectcheck_backend.dao.DAO;
+import nl.han.oose.buizerd.projectcheck_backend.domain.Begeleider;
+import nl.han.oose.buizerd.projectcheck_backend.domain.Deelnemer;
+import nl.han.oose.buizerd.projectcheck_backend.domain.Kamer;
+import nl.han.oose.buizerd.projectcheck_backend.domain.KamerFase;
+
+/**
+ * Zet de fase van de kamer waaraan de begeleider deelneemt
+ * naar de volgende fase en geeft deze terug.
+ * <p>
+ * Als de deelnemer geen begeleider is, dan wordt er een
+ * {@code VERBODEN} status teruggegeven.
+ */
+public class VolgendeFaseEvent extends Event {
+
+	@Override
+	protected EventResponse voerUit(Deelnemer deelnemer, Session session) {
+		if (deelnemer instanceof Begeleider) {
+			Kamer kamer = deelnemer.getKamer();
+			KamerFase kamerFase = kamer.getKamerFase();
+			deelnemer.getKamer().setKamerFase(kamerFase.volgendeFase());
+
+			return new EventResponse(EventResponse.Status.OK).metContext("volgendeFase", kamer.getKamerFase());
+		}
+
+		return new EventResponse(EventResponse.Status.VERBODEN);
+	}
+
+	@Override
+	protected void handelAf(DAO<Kamer, String> kamerDAO, Kamer kamer) {
+		kamerDAO.update(kamer);
+	}
+
+}
---
+++ src/test/java/nl/han/oose/buizerd/projectcheck_backend/event/VolgendeFaseEventTest.java
@@ -0,0 +1,66 @@
+package nl.han.oose.buizerd.projectcheck_backend.event;
+
+import jakarta.websocket.Session;
+import nl.han.oose.buizerd.projectcheck_backend.dao.DAO;
+import nl.han.oose.buizerd.projectcheck_backend.domain.Begeleider;
+import nl.han.oose.buizerd.projectcheck_backend.domain.Deelnemer;
+import nl.han.oose.buizerd.projectcheck_backend.domain.Kamer;
+import nl.han.oose.buizerd.projectcheck_backend.domain.KamerFase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class VolgendeFaseEventTest {
+
+	private VolgendeFaseEvent volgendeFaseEvent;
+
+	@BeforeEach
+	void setUp() {
+		volgendeFaseEvent = new VolgendeFaseEvent();
+	}
+
+	@Test
+	void handelAf_updateKamer(@Mock DAO<Kamer, String> kamerDAO, @Mock Kamer kamer) {
+		volgendeFaseEvent.handelAf(kamerDAO, kamer);
+		Mockito.verify(kamerDAO).update(kamer);
+	}
+
+	@Nested
+	class voerUit {
+
+		@Mock
+		private Session session;
+
+		@Test
+		void deelnemerIsBegeleider_zetKamerFaseNaarVolgendeFaseEnGeeftJuisteEventResponseTerug(
+			@Mock Begeleider begeleider, @Mock Kamer kamer, @Mock KamerFase kamerFase
+		) {
+			Mockito.when(begeleider.getKamer()).thenReturn(kamer);
+			Mockito.when(kamer.getKamerFase()).thenReturn(kamerFase);
+
+			EventResponse eventResponse = volgendeFaseEvent.voerUit(begeleider, session);
+
+			Assertions.assertAll(
+				() -> {
+					KamerFase expectedKamerFase = Mockito.verify(kamerFase).volgendeFase();
+					Mockito.verify(kamer).setKamerFase(expectedKamerFase);
+				},
+				() -> Assertions.assertEquals(EventResponse.Status.OK, eventResponse.getStatus()),
+				() -> Assertions.assertEquals(kamer.getKamerFase(), eventResponse.getContext().get("volgendeFase"))
+			);
+		}
+
+		@Test
+		void deelnemerIsNietBegeleider_geeftJuisteEventResponseTerug(@Mock Deelnemer deelnemer) {
+			Assertions.assertEquals(EventResponse.Status.VERBODEN, volgendeFaseEvent.voerUit(deelnemer, session).getStatus());
+		}
+
+	}
+
+}
