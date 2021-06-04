5c38fca909f494c8908a76a5796c194fbdd2b17d
LukaVri <L.vanRingelestein@student.han.nl>
2021-06-04 10:46:11 +0200

    Update voor imports

--- src/main/java/nl/han/oose/buizerd/projectcheck_backend/domain/Deelnemer.java
+++ src/main/java/nl/han/oose/buizerd/projectcheck_backend/domain/Deelnemer.java
@@ -39,7 +39,7 @@ public class Deelnemer {
 	@Valid
 	@EmbeddedId
 	private DeelnemerId deelnemerId;
-	private Rol rol;
+
 
 
 	@Expose
--- src/main/java/nl/han/oose/buizerd/projectcheck_backend/domain/KaartToelichting.java
+++ src/main/java/nl/han/oose/buizerd/projectcheck_backend/domain/KaartToelichting.java
@@ -1,5 +1,6 @@
 package nl.han.oose.buizerd.projectcheck_backend.domain;
 
+import com.google.gson.annotations.Expose;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
