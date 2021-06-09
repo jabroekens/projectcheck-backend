4b3b9e3d635fcb1e9fa2c95feab8b4e941462cef
JA Broekens <JA.Broekens@student.han.nl>
2021-06-09 17:17:40 +0200

    fix existing and add new unit tests

--- src/test/java/nl/han/oose/buizerd/projectcheck_backend/event/HighlightKaartEventTest.java
+++ src/test/java/nl/han/oose/buizerd/projectcheck_backend/event/HighlightKaartEventTest.java
@@ -1,72 +1,88 @@
 package nl.han.oose.buizerd.projectcheck_backend.event;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import jakarta.websocket.Session;
+import java.util.Optional;
 import nl.han.oose.buizerd.projectcheck_backend.domain.Deelnemer;
-import nl.han.oose.buizerd.projectcheck_backend.domain.Kaart;
 import nl.han.oose.buizerd.projectcheck_backend.domain.KaartToelichting;
 import nl.han.oose.buizerd.projectcheck_backend.domain.Kamer;
 import nl.han.oose.buizerd.projectcheck_backend.domain.Ronde;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-public class HighlightKaartEventTest {
+@ExtendWith(MockitoExtension.class)
+class HighlightKaartEventTest {
 
-	private HighlightenKaartEvent highlightenKaartEvent;
-	private Session session;
-	private Kamer kamer;
-	private Deelnemer deelnemer;
+	@Mock
 	private KaartToelichting kaartToelichting;
-	private Kaart kaart;
-	private Ronde ronde;
+
+	private HighlightenKaartEvent sut;
 
 	@BeforeEach
-	void setup() {
-		session = Mockito.mock(Session.class);
-		kamer = Mockito.mock(Kamer.class);
-		deelnemer = Mockito.mock(Deelnemer.class);
-		kaart = Mockito.mock(Kaart.class);
-		ronde = new Ronde();
-
-	}
-
-	/**
-	 * In deze test word highlighten kaart uitgevoert met een toelichting erbij.
-	 * Er wordt vervolgend gekeken of in het antwoord de zelfde toelichting zit en dat in de ronde deze kaart met toelichting zit.
-	 * Zo kan er worden gekeken of de kaart wordt gehighlight en of de toelichting correct wordt gezet.
-	 */
-
-	@Test
-	void kaartKanWordenGehighlightTest() {
-		//Arrange
-		String expectedToelichting = "Testen";
-		kaartToelichting = new KaartToelichting(kaart, expectedToelichting);
-		highlightenKaartEvent = new HighlightenKaartEvent();
-		highlightenKaartEvent.kaartToelichting = kaartToelichting;
-		Mockito.when(kamer.getHuidigeRonde()).thenReturn(java.util.Optional.ofNullable(ronde));
-		Mockito.when(deelnemer.getKamer()).thenReturn(kamer);
-		//Act
-		EventResponse antwoord = highlightenKaartEvent.voerUit(deelnemer, session);
-		KaartToelichting kaartUitAntwoord = (KaartToelichting) antwoord.getContext().get("gehighlighteKaart");
-		String actualToelichting = kaartUitAntwoord.getToelichting();
-		//Assert
-		Assertions.assertEquals(expectedToelichting, actualToelichting);
-		Assertions.assertEquals(expectedToelichting, ronde.getGehighlighteKaart().get().getToelichting());
-
+	void setUp() {
+		sut = new HighlightenKaartEvent();
+		sut.kaartToelichting = kaartToelichting;
 	}
 
 	@Test
-	void testRondeNietGevonden() {
-		//Arrange
-		highlightenKaartEvent = new HighlightenKaartEvent();
-		Mockito.when(deelnemer.getKamer()).thenReturn(kamer);
+	void voerUit_huidigeRondeAfwezig_stuurtJuisteEventResponse(@Mock Deelnemer deelnemer, @Mock Session session, @Mock Kamer kamer) {
+		when(deelnemer.getKamer()).thenReturn(kamer);
+		when(kamer.getHuidigeRonde()).thenReturn(Optional.empty());
 
-		//Act
-		EventResponse response = highlightenKaartEvent.voerUit(deelnemer, session);
+		var eventResponse = sut.voerUit(deelnemer, session);
 
-		//Assert
-		Assertions.assertEquals(response.getStatus(), EventResponse.Status.RONDE_NIET_GEVONDEN);
+		assertEquals(EventResponse.Status.RONDE_NIET_GEVONDEN, eventResponse.getStatus());
+	}
+
+	@Nested
+	class voerUit_huidigeRondeAanwezig {
+
+		@Mock
+		private Deelnemer deelnemer;
+
+		@Mock
+		private Session session;
+
+		@Mock
+		private Ronde ronde;
+
+		@BeforeEach
+		void setUp(@Mock Kamer kamer) {
+			when(deelnemer.getKamer()).thenReturn(kamer);
+			when(kamer.getHuidigeRonde()).thenReturn(Optional.of(ronde));
+		}
+
+		@Test
+		void magNietHighlighten_geeftJuisteEventResponse(@Mock KaartToelichting kaartToelichting) {
+			when(ronde.getGehighlighteKaart()).thenReturn(Optional.of(kaartToelichting));
+
+			var eventResponse = sut.voerUit(deelnemer, session);
+
+			assertEquals(EventResponse.Status.VERBODEN, eventResponse.getStatus());
+		}
+
+		@Test
+		void magHighlighten_zetGehighlighteKaart() {
+			when(ronde.getGehighlighteKaart()).thenReturn(Optional.empty());
+
+			var eventResponse = sut.voerUit(deelnemer, session);
+
+			assertAll(
+				() -> verify(ronde).setGehighlighteKaart(kaartToelichting),
+				() -> assertEquals(kaartToelichting, eventResponse.getContext().get("gehighlighteKaart")),
+				() -> assertTrue(eventResponse.isStuurNaarAlleClients()),
+				() -> assertEquals(EventResponse.Status.OK, eventResponse.getStatus())
+			);
+		}
 
 	}
 
--- src/test/java/nl/han/oose/buizerd/projectcheck_backend/event/HighlightVolgendeKaartEventTest.java
+++ src/test/java/nl/han/oose/buizerd/projectcheck_backend/event/HighlightVolgendeKaartEventTest.java
@@ -1,77 +1,79 @@
 package nl.han.oose.buizerd.projectcheck_backend.event;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import jakarta.websocket.Session;
 import java.util.Optional;
 import nl.han.oose.buizerd.projectcheck_backend.domain.Begeleider;
 import nl.han.oose.buizerd.projectcheck_backend.domain.Deelnemer;
-import nl.han.oose.buizerd.projectcheck_backend.domain.Kaart;
-import nl.han.oose.buizerd.projectcheck_backend.domain.KaartToelichting;
 import nl.han.oose.buizerd.projectcheck_backend.domain.Kamer;
 import nl.han.oose.buizerd.projectcheck_backend.domain.Ronde;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-public class HighlightVolgendeKaartEventTest {
+@ExtendWith(MockitoExtension.class)
+class HighlightVolgendeKaartEventTest {
 
-	private HighlightVolgendeKaartEvent highlightVolgendeKaartEvent;
-	private HighlightenKaartEvent highlightenKaartEvent;
-	private Session session;
-	private Kamer kamer;
-	private Deelnemer deelnemer;
-	private KaartToelichting kaartToelichting;
-
-	private Kaart kaart;
-	private Ronde ronde;
+	private HighlightVolgendeKaartEvent sut;
 
 	@BeforeEach
-	void setup() {
-		session = Mockito.mock(Session.class);
-		kamer = Mockito.mock(Kamer.class);
-		deelnemer = Mockito.mock(Begeleider.class);
-		kaart = Mockito.mock(Kaart.class);
-		ronde = new Ronde();
-
-	}
-
-	/**
-	 * In deze test word eerst de kaart toelichting gezet, om vervolgens te kijken of deze weer null wordt,
-	 * door het aanroepen van highlighten volgende kaart.
-	 * Op deze manier kan er worden gekeken of dit event werkt.
-	 */
-	@Test
-	void VolgendeKaartKanWordenGehighlight() {
-		//Arrange
-		highlightVolgendeKaartEvent = new HighlightVolgendeKaartEvent();
-		highlightenKaartEvent = new HighlightenKaartEvent();
-
-		Mockito.when(kamer.getHuidigeRonde()).thenReturn(Optional.ofNullable(ronde));
-		Mockito.when(deelnemer.getKamer()).thenReturn(kamer);
-
-		highlightenKaartEvent.kaartToelichting = Mockito.mock(KaartToelichting.class);
-
-		//Act
-
-		highlightenKaartEvent.voerUit(deelnemer, session);
-		highlightVolgendeKaartEvent.voerUit(deelnemer, session);
-
-		//Assert
-		Assertions.assertEquals(Optional.empty(), ronde.getGehighlighteKaart());
-
+	void setUp() {
+		sut = new HighlightVolgendeKaartEvent();
 	}
 
 	@Test
-	void testRondeNietGevonden() {
-		//Arrange
-		highlightVolgendeKaartEvent = new HighlightVolgendeKaartEvent();
-		Mockito.when(deelnemer.getKamer()).thenReturn(kamer);
+	void voerUit_deelnemerIsGeenBegeleider_stuurtJuisteEventResponse(@Mock Deelnemer deelnemer, @Mock Session session) {
+		var eventResponse = sut.voerUit(deelnemer, session);
+		assertEquals(EventResponse.Status.VERBODEN, eventResponse.getStatus());
+	}
 
-		//Act
-		EventResponse response = highlightVolgendeKaartEvent.voerUit(deelnemer, session);
+	@Nested
+	class voerUit_deelnemerIsBegeleider {
 
-		//Assert
-		Assertions.assertEquals(response.getStatus(), EventResponse.Status.RONDE_NIET_GEVONDEN);
+		@Mock
+		private Begeleider begeleider;
+
+		@Mock
+		private Session session;
+
+		@Mock
+		private Kamer kamer;
+
+		@BeforeEach
+		void setUp() {
+			when(begeleider.getKamer()).thenReturn(kamer);
+		}
+
+		@Test
+		void huidigeRondeAfwezig_stuurtJuisteEventResponse() {
+			when(kamer.getHuidigeRonde()).thenReturn(Optional.empty());
+
+			var eventResponse = sut.voerUit(begeleider, session);
+
+			assertEquals(EventResponse.Status.RONDE_NIET_GEVONDEN, eventResponse.getStatus());
+		}
+
+		@Test
+		void huidigeRondeAanwezig_stuurtJuisteEventResponse(@Mock Ronde ronde) {
+			when(kamer.getHuidigeRonde()).thenReturn(Optional.of(ronde));
+
+			var eventResponse = sut.voerUit(begeleider, session);
+
+			assertAll(
+				() -> verify(ronde).setGehighlighteKaart(null),
+				() -> assertNull(eventResponse.getContext().get("gehighlighteKaart")),
+				() -> assertTrue(eventResponse.isStuurNaarAlleClients())
+			);
+		}
 
 	}
 
