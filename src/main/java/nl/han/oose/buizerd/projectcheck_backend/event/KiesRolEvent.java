8f1f389cb97cf49746f8e923054e34c93b87e426
Daniël <DJ.Vervloed@student.han.nl>
2021-06-09 14:27:17 +0200

    Letterlijk de naam

--- src/main/java/nl/han/oose/buizerd/projectcheck_backend/event/KiesRolEvent.java
+++ src/main/java/nl/han/oose/buizerd/projectcheck_backend/event/KiesRolEvent.java
@@ -3,7 +3,9 @@ package nl.han.oose.buizerd.projectcheck_backend.event;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.websocket.Session;
+import nl.han.oose.buizerd.projectcheck_backend.dao.DAO;
 import nl.han.oose.buizerd.projectcheck_backend.domain.Deelnemer;
+import nl.han.oose.buizerd.projectcheck_backend.domain.Kamer;
 import nl.han.oose.buizerd.projectcheck_backend.domain.Rol;
 
 public class KiesRolEvent extends Event {
@@ -16,6 +18,11 @@ public class KiesRolEvent extends Event {
 	protected EventResponse voerUit(Deelnemer deelnemer, Session session) {
 		deelnemer.setRol(rol);
 		return new EventResponse(EventResponse.Status.OK).metContext("gekozenRol", rol);
+	}
+
+	@Override
+	protected void handelAf(DAO<Kamer, String> kamerDAO, Kamer kamer) {
+		kamerDAO.update(kamer);
 	}
 
 }
